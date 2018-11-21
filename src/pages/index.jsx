2bd6d723edc1f588dfec539081d43f49f18ab181
George.Rodier <grodier3@gmail.com>
2018-11-21 16:44:52 -0500

    Add initial page layout

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
 
 function Hello() {
-  return <div>Hello world!</div>;
+  return (
+    <>
+      <header>
+        <h1>Grace's Favorite Commercials</h1>
+      </header>
+      <main>
+        <section id="videoPlayer">
+          <h2>Pig Commercial</h2>
+          <p>Santander Pig commercial</p>
+        </section>
+        <section id="commercialList">
+          <h2>Commercials</h2>
+          <ul>
+            <li>
+              <h3>Frankenstein</h3>
+              <p>Apple Holiday Frankenstein commercial</p>
+            </li>
+            <li>
+              <h3>Ahoy</h3>
+              <p>Bounty Gotcha</p>
+            </li>
+          </ul>
+        </section>
+      </main>
+    </>
+  );
 }
 
 export default Hello;
