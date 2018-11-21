1e1a126ee377010cdc31605d3d98c8f484478b34
George Rodier <grodier3@gmail.com>
2018-11-20 20:58:29 -0500

    Add eslint

---
+++ gatsby-conifg.js
@@ -0,0 +1,15 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-eslint',
+      options: {
+        test: /\.js$|\.jsx$/,
+        exclude: /(node_modules|cache|public)/,
+        options: {
+          emitWarning: true,
+          failOnError: true
+        }
+      }
+    }
+  ]
+}
