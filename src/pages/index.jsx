c1a96d0028ab5ef1c8c434cda82219491edb826d
George Rodier <grodier3@gmail.com>
2018-11-21 19:57:54 -0500

    Add more content to initial layout

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -10,16 +10,33 @@ function Hello() {
         <section id="videoPlayer">
           <h2>Pig Commercial</h2>
           <p>Santander Pig commercial</p>
+          <iframe
+            title="sanataderpig"
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/W27bN29GFhs"
+            frameBorder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          />
         </section>
         <section id="commercialList">
           <h2>Commercials</h2>
           <ul>
             <li>
               <h3>Frankenstein</h3>
+              <img
+                alt="Santader pig thumbnail"
+                src="https://img.youtube.com/vi/W27bN29GFhs/hqdefault.jpg"
+              />
               <p>Apple Holiday Frankenstein commercial</p>
             </li>
             <li>
               <h3>Ahoy</h3>
+              <img
+                alt="Santader pig thumbnail"
+                src="https://img.youtube.com/vi/W27bN29GFhs/hqdefault.jpg"
+              />
               <p>Bounty Gotcha</p>
             </li>
           </ul>
