53e633b7cee45fa3fca15a338d1ae8353eb7ee46
George Rodier <grodier3@gmail.com>
2018-11-24 20:07:35 -0500

    Add hover elements to commercial list

--- src/components/commercialList.jsx
+++ src/components/commercialList.jsx
@@ -1,6 +1,52 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const ListTitle = styled.h3`
+  position: relative;
+  margin: 0 auto;
+  top: 50%;
+  color: white;
+  visibility: hidden;
+  text-align: center;
+`;
+
+const ListImg = styled.img`
+  display: block;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  margin-top: -180px;
+  margin-left: -240px;
+`;
+
+const ListAnchor = styled.a`
+  position: relative;
+  display: block;
+  width: 100%;
+  height: 100%;
+
+  :visited,
+  :link,
+  :active {
+    text-decoration: none;
+  }
+
+  :hover,
+  :focus {
+    background-color: #1f1645;
+    opacity: 1;
+  }
+
+  :hover ${ListTitle}, :focus ${ListTitle} {
+    visibility: visible;
+    opacity: 1;
+  }
+
+  :hover ${ListImg}, :focus ${ListImg} {
+    opacity: 0.2;
+  }
+`;
+
 const ListItem = styled.li`
   width: 230px;
   height: 230px;
@@ -11,38 +57,12 @@ const ListItem = styled.li`
   box-shadow: 0px 3px 15px rgba(0, 0, 0, 0.2);
 `;
 
-const ListAnchor = styled.a`
-  position: relative;
-  display: block;
-  width: 100%;
-  height: 100%;
-
-  :hover,
-  :focus {
-    background-color: #1f1645;
-    opacity: 1;
-  }
-`;
-
-const ListImg = styled.img`
-  display: block;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  margin-top: -180px;
-  margin-left: -240px;
-
-  :hover,
-  :focus {
-    opacity: 0.45;
-  }
-`;
-
 function CommercialItem({commercial}) {
   return (
     <ListItem>
       <ListAnchor href={commercial.youtubeLink}>
         <ListImg alt={commercial.title} src={commercial.thumbnail} />
+        <ListTitle>{commercial.title}</ListTitle>
       </ListAnchor>
     </ListItem>
   );
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -17,15 +17,21 @@ function Main() {
       youtubeLink: 'https://youtube.com',
     },
     {
-      id: 'W27bN29GFhs',
-      title: 'Enterprise Laugh',
-      thumbnail: 'https://img.youtube.com/vi/W27bN29GFhs/hqdefault.jpg',
+      id: 'rYzCJ24qJj4',
+      title: "Enterprise: We've Got It Covered",
+      thumbnail: 'https://img.youtube.com/vi/rYzCJ24qJj4/hqdefault.jpg',
       youtubeLink: 'https://youtube.com',
     },
     {
       id: 'W27bN29GFhs',
       title: 'Santander Pig',
       thumbnail: 'https://img.youtube.com/vi/W27bN29GFhs/hqdefault.jpg',
+      youtubeLink: 'https://youtube.com',
+    },
+    {
+      id: 'Om3MeiTtLfo',
+      title: 'Progressive: Cycling Is My Passion',
+      thumbnail: 'https://img.youtube.com/vi/Om3MeiTtLfo/hqdefault.jpg',
       youtubeLink: 'https://youtube.com',
     },
   ];
