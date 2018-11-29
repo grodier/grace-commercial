4a3a35d81b819e31e7af70d507e52a8697d56188
George.Rodier <grodier3@gmail.com>
2018-11-29 17:31:19 -0500

    Limit content area of responsive video

--- src/components/videoSection.jsx
+++ src/components/videoSection.jsx
@@ -4,36 +4,46 @@ import styled from 'styled-components';
 // The following div and ifrome styles are a way to incorporate
 // a responsive video. Technique to do this taken from:
 // https://css-tricks.com/NetMag/FluidWidthVideo/Article-FluidWidthVideo.php
+
 const VidFrame = styled.iframe`
   position: absolute;
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
+  max-height: 540px;
 `;
 
-const VidWrapper = styled.div`
+const VidResponsiveWrapper = styled.div`
   position: relative;
   padding-bottom: 56.25%;
   padding-top: 25px;
   height: 0;
 `;
 
+const VidContentArea = styled.div`
+  max-width: 960px;
+  max-height: 540px;
+  margin: 0 auto;
+`;
+
 function VideoSection({commercial}) {
   return (
     <section>
       <h2>{commercial.title}</h2>
-      <VidWrapper>
-        <VidFrame
-          title={commercial.title}
-          width="853"
-          height="505"
-          src={`https://www.youtube.com/embed/${commercial.id}`}
-          frameBorder="0"
-          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        />
-      </VidWrapper>
+      <VidContentArea>
+        <VidResponsiveWrapper>
+          <VidFrame
+            title={commercial.title}
+            width="853"
+            height="505"
+            src={`https://www.youtube.com/embed/${commercial.id}`}
+            frameBorder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          />
+        </VidResponsiveWrapper>
+      </VidContentArea>
     </section>
   );
 }
