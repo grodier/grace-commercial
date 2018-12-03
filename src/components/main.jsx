64c09b956f54e418c2a831c9220de2bc3f2b14b7
George Rodier <grodier3@gmail.com>
2018-12-02 22:48:52 -0500

    Fix build issue by moving polyfill to onClientEntry

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
-import smoothscroll from 'smoothscroll-polyfill';
 import VideoSection from './videoSection';
 import CommercialList from './commercialList';
-
-smoothscroll.polyfill();
 
 const commercials = [
   {
